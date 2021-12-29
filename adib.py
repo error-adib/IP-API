b4c137e8c69571f44adf413b2113577b2e567c4e
Adnan Adib <68530121+error-adib@users.noreply.github.com>
2021-12-29 19:20:17 +0600

    Update adib.py

--- adib.py
+++ adib.py
@@ -1,40 +1,30 @@
-import pyfiglet
-import os
 from requests import *
+import socket
 
 ip = get('https://api.ipify.org/').text
-help = '''
+uname = socket.gethostname()
+maddr = socket.gethostbyname(uname)
 
- [1] ip    - GET UR IP ADDRESS
- [2] about - About this script
- [3] exit  - EXIT THE SCRIPT
-
+INFO = '''
+ 
+                         ______   
+  ____________    _____|\     \  
+ /            \  /     / |     | 
+|\___/\  \\___/||      |/     /| 
+ \|____\  \___|/|      |\____/ | 
+       |  |     |\     \    | /  
+  __  /   / __  | \     \___|/   
+ /  \/   /_/  | |  \     \       
+|____________/|  \  \_____\      
+|           | /   \ |     |      
+|___________|/     \|_____| 
+ 
+ 
 '''
 
-txt = pyfiglet.figlet_format("Error", font="graffiti")
-print(txt)
+print("\u001b[32;1m ", INFO)
 
-print("\u001b[32;1m Programmed By Adnan Adib")
-
-con = True
-while con:
-    user = input("error >> ")
-
-    if user=='ip':
-        print("Your Public IP is: ", ip)
-    
-    elif user=='about':
-        print("No INFO TO SHOW")
-    
-    elif user=='help':
-        print("\u001b[36;1m ",help)
-    
-    elif user=='exit':
-        con = False
-
-    elif user=='cmd':
-        os.system("start cmd")
-
-    else:
-        print("wrong command")
+print("\u001b[36;1m Your Uname : ", uname)
+print("\u001b[36;1m Your public IP is : ", ip)
+print("\u001b[36;1m Your PC's address : ", maddr)
 
